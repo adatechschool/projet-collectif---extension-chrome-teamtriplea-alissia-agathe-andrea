080353126e659aeb45a7af83a5cfa3be5d2e91b9
Andréa Bacquelé <bacquelea@gmail.com>
2023-05-04 14:17:19 +0200

    Bouton SOS

---
+++ FindImage.js
@@ -0,0 +1,10 @@
+// Récupere les éléments img grace à querySelectorAll
+// Pour chaque element récupéré : Mettre le bouton SOS dessus
+
+function findImage(){
+let allImages = document.querySelectorAll("img");
+console.log(allImages)
+}
+
+let allImages = document.querySelectorAll("img");
+console.log(allImages)
