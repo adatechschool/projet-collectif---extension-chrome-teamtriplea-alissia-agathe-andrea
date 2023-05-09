273305fbb9d0567bb180b2d2649c68818c72c30d
Andréa Bacquelé <bacquelea@gmail.com>
2023-05-09 10:57:50 +0200

    changement méthodes pour image

--- fonctionnalite_images/ReplaceImage.js
+++ fonctionnalite_images/ReplaceImage.js
@@ -1,7 +1,7 @@
 function findImg(){
 //let allImages = document.querySelectorAll("img");
 let allImages = document.getElementsByTagName('img')
-console.log(allImages)
+//console.log(allImages)
     return allImages
 }
 
@@ -11,20 +11,26 @@ console.log(allImages)
 
 function imageChange(){
   console.log('images changing');
-  let filenames = ['IconeRemplacement.png','sos.png'];
+  let filenames = ['images/IconeRemplacement.png','images/sos.png'];
   //let filenames = ['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5qRC4YCAgWeXtRA7vu6KMtkKCsVA8h1FUW81szGZR0A&s']
   let images = document.getElementsByTagName('img')
-  console.log(images)
-  for(imgElt of images){
-    console.log(imgElt.src)
-    let test = Math.floor(Math.random() * filenames.length);
-    let file = filenames[test]
-    let url = chrome.runtime.getURL(file);
-    console.log(url)
-    // utilisation createElement et créer une nouvelle balise img
-    imgElt.src = url;
+  let test = Math.floor(Math.random() * filenames.length);
+  for (let i = 0; i < images.length; i++){
+    images[i].src = filenames[test];
+    test++;
+    if (test > 8){
+      random = 0;
+    }
   }
-}
+  //for(imgElt of images){
+    //console.log(imgElt.src)
+    //let test = Math.floor(Math.random() * filenames.length);
+    //let file = filenames[test]
+    //let url = chrome.runtime.getURL(file);
+    //console.log(url)
+    // utilisation createElement et créer une nouvelle balise img
+    //imgElt.src = url;
+  }
 
 imageChange()
 
