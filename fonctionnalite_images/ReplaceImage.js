f39c7d8eb1f9c2c415c6b2f804365331a2d6a4be
Andréa Bacquelé <bacquelea@gmail.com>
2023-05-09 15:05:35 +0200

    Remplacement de l'image opérationnelle et reliée à l'API

--- fonctionnalite_images/ReplaceImage.js
+++ fonctionnalite_images/ReplaceImage.js
@@ -1,40 +1,6 @@
 
-function imageChange(){
-  console.log('images changing');
-  //let filenames = ['images/IconeRemplacement.png','images/sos.png'];
-  let filenames = [
-    'https://www.referenseo.com/wp-content/uploads/2019/03/image-attractive-960x540.jpg',
-    'https://cdn.futura-sciences.com/sources/images/dossier/773/01-intro-773.jpg'
-  ]
-  let images = document.getElementsByTagName('img')
-  for (let i = 0; i < images.length; i++){
-  let random = Math.floor(Math.random() * filenames.length);
-  let file = filenames[random]
-  images[i].src = file  
-  }
-  //for(imgElt of images){
-    //console.log(imgElt.src)
-    //let test = Math.floor(Math.random() * filenames.length);
-    //let file = filenames[test]
-    //let url = chrome.runtime.getURL(file);
-    //console.log(url)
-    // utilisation createElement et créer une nouvelle balise img
-    //imgElt.src = url;
-  }
 
-imageChange()
-
-//function buttonImage(){
-//  let images = findImg()
-//  for (let i = 0; i < images.length; i++){
-        //let button = document.createElement('input')
-        // inserer une div avec un id juste avant l'image
-   //     images.insertAdjacentHTML('afterend', '<div "')
- //   }
-//}
-
-//buttonImage()
-
+// Appel de l'API qui génére une image aléatoire d'un insecte préalablement demandé à l'utilisateur + changement de l'image
 async function imageApi(){
 
   let theme = prompt("Quel thème voulez-vous ?")
@@ -47,11 +13,17 @@ async function imageApi(){
   let formation = [];
 
   for (const image of array){
-    formation.push(image.webformatURL)
+    console.log(formation.push(image.webformatURL))
   }
 
-  const randomImage = Math.floor(Math.random()*formation.length);
-  document.getElementsByTagName("img").src = formation[randomImage]
+  let images = document.getElementsByTagName('img')
+  let randomIndex = Math.floor(Math.random() * formation.length);
+  for (let i = 0; i < images.length; i++){
+  images[i].src = formation[randomIndex]
+  }
+
+  //const randomImage = Math.floor(Math.random()*formation.length);
+  //document.getElementsByTagName("img").src = formation[randomImage]
 }
 
 imageApi()
